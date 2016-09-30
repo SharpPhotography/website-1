d888f364ad76757a98870f6672ccf24dedf0b6b0
Derek-Sharp <dsharp640@comcast.net>
2016-09-30 16:25:51 -0600

    Update PageCreate.js

--- PageCreate.js
+++ PageCreate.js
@@ -1,5 +1,5 @@
 var pageHeader = document.getElementsByTagName("header");
 var headerJumbo = document.createElement("div").classList.add("jumbotron", "jtTest");
-headerJumbo.appendChild(pageHeader);
+headerJumbo.appendChild(pageHeader[0]);
 
 
