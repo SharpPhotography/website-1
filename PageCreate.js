6999e1bf45874357f037a36682785900f3faf627
Derek-Sharp <dsharp640@comcast.net>
2016-09-30 16:50:32 -0600

    Update PageCreate.js

--- PageCreate.js
+++ PageCreate.js
@@ -1,5 +1,18 @@
-var headerJumbo = document.createElement("DIV");
-headerJumbo.classList.add("jumbotron", "jtTest");
-document.getElementById("header").appendChild(headerJumbo);
+document.getElementById("header").innerHTML = "<div class="jumbotron jtTest">
+</div>
+<div class="container-fluid">
+<div class="row headerdiv sectiontitle">
+<div class="col-md-3 col-md-offset-1 col-xs-10 col-xs-offset-1 text-center text-nowrap WTBS2 websitetitle">
+<h1>Auburn Studios</h1>
+</div>
+<div class="col-md-8">
+<div class="row">
+<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>GALLERIES</h4></div>
+<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>NEWS</h4></div>
+<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>ABOUT</h4></div>
+<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>CONTACT</h4></div>
+</div>
+</div>
+</div>";
 
 
