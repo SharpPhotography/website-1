5e431d766da22406318fb0ca8d5550e3607c8a74
Derek-Sharp <dsharp640@comcast.net>
2016-10-29 15:04:41 -0600

    Update PageCreate.js

--- PageCreate.js
+++ PageCreate.js
@@ -1,18 +1,3 @@
-document.getElementById("header").innerHTML = '<div class="jumbotron jtTest">
-</div>
-<div class="container-fluid">
-<div class="row headerdiv sectiontitle">
-<div class="col-md-3 col-md-offset-1 col-xs-10 col-xs-offset-1 text-center text-nowrap WTBS2 websitetitle">
-<h1>Auburn Studios</h1>
-</div>
-<div class="col-md-8">
-<div class="row">
-<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>GALLERIES</h4></div>
-<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>NEWS</h4></div>
-<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>ABOUT</h4></div>
-<div class="col-md-2 col-xs-3 orangeGradient WTBS headertab text-center"><h4>CONTACT</h4></div>
-</div>
-</div>
-</div>';
-
+var $header = $('#header');
+alert(header);
 
