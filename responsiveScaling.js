71895cad0edb3dc6a954c94170a3f38e7a03f4cd
Derek-Sharp <dsharp640@comcast.net>
2016-11-03 22:33:56 -0600

    Add files via upload

--- responsiveScaling.js
+++ responsiveScaling.js
@@ -1,18 +1,19 @@
 var $H1 = $("h1");
+var $H3 = $("h3");
 var $H4 = $("h4");
 var $P = $("p");
 function doSizing() {
   if (screen.width >= 990) {
     $H1.css("font-size", 36);
+    $H3.css("font-size", 24);
     $H4.css("font-size", 18);
     $P.css("font-size", 14);
-  } else if (screen.width <= 400) {
-    $H1.css("font-size", 72);
-    $H4.css("font-size", 36);
-    $P.css("font-size", 28);
+  } else if (screen.width <= 990) {
+    $H1.css("font-size", Math.floor((1/screen.width)*36000));
+    $H3.css("font-size", Math.floor((1/screen.width)*24000));
+    $H4.css("font-size", Math.floor((1/screen.width)*18000));
+    $P.css("font-size", Math.floor((1/screen.width)*14000));
   }
 }
-while (screen.width === screen.width) {
-  doSizing();
-}
+setInterval(function(){doSizing()},0);
 $(document).ready(doSizing);
