bb5f248133c2815e47ff9961b6d9f03245bf1481
Derek-Sharp <dsharp640@comcast.net>
2016-11-28 23:49:27 -0700

    Update galleryFunction.js

--- scripts/galleryFunction.js
+++ scripts/galleryFunction.js
@@ -1,14 +1,17 @@
-var $Image = $(".imageContainer");
 var galleryContainer = document.getElementById("galleryContainer");
 
 $(document).ready(function(){
+  var $Image = $(".imageContainerFunction");
   $Image.click(function() {
     $("#ExpandedPhoto").remove();
+    var ExpandedPhotoLink = document.createElement("a");
     var ExpandedPhoto = document.createElement("img");
     ExpandedPhoto.className = "imageContainer activeImage darkGrey";
-    ExpandedPhoto.id = "ExpandedPhoto";
+    ExpandedPhotoLink.id = "ExpandedPhoto";
+    ExpandedPhotoLink.href = $(this).children().attr("src");
     ExpandedPhoto.src = $(this).children().attr("src");
-    galleryContainer.appendChild(ExpandedPhoto);
+    galleryContainer.appendChild(ExpandedPhotoLink);
+    ExpandedPhotoLink.appendChild(ExpandedPhoto);
     var greyBox = document.createElement("div");
     greyBox.className="greyBox";
     galleryContainer.appendChild(greyBox);
@@ -16,8 +19,8 @@ $(document).ready(function(){
 });
 setInterval(function() {
   $(document).ready(function() {
-    $("#ExpandedPhoto").click(function() {
-      $(".greyBox").remove();
+    $(".greyBox").click(function() {
+      $("#ExpandedPhoto").remove();
       $(this).remove();
     });
   });
